aa883c4cc300eac5e03c520eab04a40d16b7e886
pixarusemperor <pixarusemperor@users.noreply.github.com>
2026-07-02 11:12:13 +0000

    chore: set no-cache headers in next.config to bust long s-maxage for seeing redesign live

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   serverExternalPackages: [],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
